bde1341367f7bfa07739981e7170c972ffe9efcc
somya15shekhar <161952905+somya15shekhar@users.noreply.github.com>
2025-07-11 00:31:00 +0530

    smaller model

--- text_summarizer.py
+++ text_summarizer.py
@@ -21,7 +21,7 @@ class TextSummarizer:
             # Use a small, fast model that works well for summarization
             self.model = pipeline(
                 "summarization",
-                model="sshleifer/distilbart-cnn-12-6",
+                model="linydub/bart-small",
                 device=-1  # Use CPU
             )
             self.model_loaded = True
