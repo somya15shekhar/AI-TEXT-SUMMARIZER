de79b7a200ec1d615471686bf411543a7550803d
somya15shekhar <161952905+somya15shekhar@users.noreply.github.com>
2025-07-11 00:14:56 +0530

    changed model again to deploy

--- text_summarizer.py
+++ text_summarizer.py
@@ -21,7 +21,7 @@ class TextSummarizer:
             # Use a small, fast model that works well for summarization
             self.model = pipeline(
                 "summarization",
-                model="distilbart-cnn-12-6",
+                model="sshleifer/distilbart-cnn-12-6",
                 device=-1  # Use CPU
             )
             self.model_loaded = True
